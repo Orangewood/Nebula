ad7b35fb622afc760feff3dd3e8f485a63421f89
James Dolan <jamesdolan1989@gmail.com>
2022-08-31 22:18:37 -0400

    Rudimentary leveling system

--- turn/research/ResearchTurn.ts
+++ turn/research/ResearchTurn.ts
@@ -1,31 +1,58 @@
 import { Research } from "../../models/actions/research/Research";
-import { HarvestPlanets, Player } from "../../models/player/Player";
+import { Player } from "../../models/player/Player";
+import { TestPlayer } from "../../models/player/TestPlayer";
 
 export default function ResearchTurn(
   playerStats: Player,
-  currentTurn: boolean
+  researchAttribute: Research
 ) {
-  function SelectResearch(researchAttribute: Research) {
-    switch (researchAttribute) {
-      case Research.PHYSICS: {
-        break;
-      }
+  const currentResearchLevel = playerStats.research[researchAttribute];
 
-      case Research.CHEMISTRY: {
-        break;
-      }
-      case Research.ASTRONOMY: {
-        break;
-      }
-      case Research.ROBOTICS: {
-        break;
-      }
-      case Research.BIOLOGY: {
-        break;
-      }
-
-      default:
-        break;
+  const currentLevelUpCost = (currentLevel: number) => {
+    let fibo = [1, 2];
+    for (var i = 2; i <= currentLevel; i++) {
+      fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
+    return fibo[currentLevel];
+  };
+
+  console.log(playerStats);
+
+  const requiredCost = currentLevelUpCost(currentResearchLevel!);
+  switch (researchAttribute) {
+    case Research.PHYSICS: {
+      if (playerStats.resources.discovery! >= requiredCost) {
+        playerStats.research.physics += 1;
+      }
+    }
+    case Research.CHEMISTRY: {
+      if (playerStats.resources.discovery! >= requiredCost) {
+        playerStats.research.chemistry += 1;
+      }
+    }
+    case Research.ASTRONOMY: {
+      if (playerStats.resources.discovery! >= requiredCost) {
+        playerStats.research.astronomy += 1;
+      }
+    }
+    case Research.ROBOTICS: {
+      if (
+        playerStats.resources.metal! >= requiredCost &&
+        playerStats.resources.energy! >= requiredCost
+      ) {
+        playerStats.research.robotics += 1;
+      }
+    }
+    case Research.BIOLOGY: {
+      if (playerStats.resources.life! >= requiredCost) {
+        playerStats.research.biology += 1;
+      }
+    }
+    default:
+      break;
   }
+
+  console.log(playerStats);
 }
+
+ResearchTurn(TestPlayer, Research.BIOLOGY);
