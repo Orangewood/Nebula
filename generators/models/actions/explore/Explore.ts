b777b1be1375e6c68424cf6c754072597ca7614c
James Dolan <jamesdolan1989@gmail.com>
2023-01-04 20:58:54 -0600

    Clean-up generators file

--- generators/models/actions/explore/Explore.ts
+++
@@ -1,9 +0,0 @@
-export enum Explore {
-  OBSERVE = "observe",
-  MOVEMENT = "movement",
-}
-
-export interface ExploreObj {
-  observe?: number;
-  movement?: number;
-}
--- generators/models/misc/EnumToDictionary.ts
+++
@@ -1,3 +0,0 @@
-export type EnumToDictionary<T extends keyof any, U> = {
-  [K in T]: U;
-};
